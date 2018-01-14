161d04b8d842fe71a2585deebc429e0974e34c60
Macguire Rintoul <mrintoul@users.noreply.github.com>
2018-01-14 11:29:33 -0800

    added recap

---
+++ src/Recap.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import fire from './fire';
+
+
+class Recap extends Component {
+
+    render() {
+       
+        return (
+            <div className="Compare">
+                <h1 className="overview-title">What Can We Do?</h1>
+            <h4>Readings</h4>
+                <p className="App-intro">
+                  <a href="https://medium.com/@kerigannon/times-up-i-was-fired-as-a-software-engineer-3414a19bbbf7">Time’s Up: I Was Fired As A Software Engineer</a>
+                </p>
+
+            </div>
+        );
+
+    }
+
+}
+
+export default Recap;
