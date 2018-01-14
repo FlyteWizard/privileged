2bce2fd8ed536ba456a0cd5097c1fd5b8e4721c7
sophia <ceasings@gmail.com>
2018-01-14 00:58:21 -0800

    added end page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import p from './p.png';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
+
 import Start from './Start';
 import Landing from './Landing';
 import Questions from './Questions';
+import End from './End';
 
 
 class App extends Component {
@@ -21,6 +23,8 @@ class App extends Component {
           <Route exact path='/' component={Landing}/>
           <Route exact path='/start' component={Start}/>
           <Route exact path='/questions' component={Questions}/>
+          <Route exact path='/end' component={End}/>
+          
         </Switch>
 
       </div>
--- src/Start.js
+++ src/Start.js
@@ -25,19 +25,6 @@ class Start extends Component {
         fire.database().ref("users/" + this.inputEl.value).set(
             {
                 username: this.inputEl.value,
-                q1: '',
-                q2: '',
-                q3: '',
-                q4: '',
-                q5: '',
-                q6: '',
-                q7: '',
-                q8: '',
-                q9: '',
-                q10: '',
-                q11: '',
-                q12: '',
-                
             });
         
         this.username = this.inputEl.value;
@@ -51,29 +38,29 @@ class Start extends Component {
 
   render() {
       
-      const { from } = this.props.location.state || '/'
-      const { fireRedirect } = this.state
+        const { from } = this.props.location.state || '/'
+        const { fireRedirect } = this.state
 
-    return (
-        <div className="Start">
-            <div className="start-card">
-                <h3 className="card-title">Waiting for others</h3>
-                <p className="card-intro">Need 10 participants</p>
+        return (
+            <div className="Start">
+                <div className="start-card">
+                    <h3 className="card-title">Waiting for others</h3>
+                    <p className="card-intro">Need 10 participants</p>
 
-                <h1 className="num-participants"> X </h1>
+                    <h1 className="num-participants"> X </h1>
 
-                <form className="start-form" onSubmit={this.submitForm.bind(this)}>
-                    <input className="start-name" type="text" placeholder="Your display name" ref={ el => this.inputEl = el }/>
-                    <input className ="start-button" type="submit" value="Start"/>
-                </form>
+                    <form className="start-form" onSubmit={this.submitForm.bind(this)}>
+                        <input className="start-name" type="text" placeholder="Your display name" ref={ el => this.inputEl = el }/>
+                        <input className ="start-button" type="submit" value="Start"/>
+                    </form>
 
-                {fireRedirect && (
-                  <Redirect to={from || '/questions'}/>
-                )}
+                    {fireRedirect && (
+                      <Redirect to={from || '/questions'}/>
+                    )}
+                </div>
             </div>
-        </div>
 
-    );
+        );
     }
 }
 
