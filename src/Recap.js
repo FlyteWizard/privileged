b72cfe13d736ec44961e78e0c3c04dc2c3b62be3
Amy Hanvoravongchai <amy.hanv@gmail.com>
2018-01-14 12:13:53 -0800

    Recap page styling and buttom on Compare page

--- src/Recap.js
+++ src/Recap.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 class Recap extends Component {
 
     render() {
-       
+
         return (
-            <div className="Compare">
+            <div className="Recap">
                 <h1 className="overview-title">What Can We Do?</h1>
-            <p>"Being aware of our privilege is not only a positive to those around us and those we interact with, but will make us more conscious of this fact. It will make us more flexible, more aware and more understanding." - <a href="https://hackernoon.com/tech-your-privilege-at-the-door-5d8da0c41c6b">Tech Your Privilege at the Door</a></p>
-            <h4>Readings</h4>
+                <p className="recap-text">"Being aware of our privilege is not only a positive to those around us and those we interact with, but will make us more conscious of this fact. It will make us more flexible, more aware and more understanding." - <a href="https://hackernoon.com/tech-your-privilege-at-the-door-5d8da0c41c6b">Tech Your Privilege at the Door</a></p>
+                <h3 className="card-title">Readings</h3>
                 <ul>
                   <li><a href="https://medium.com/@kerigannon/times-up-i-was-fired-as-a-software-engineer-3414a19bbbf7">Time’s Up: I Was Fired As A Software Engineer</a></li>
                 </ul>
-            <h4>Check Em' Out</h4>
-            <ul>
-            <li><a href="http://goodforpocin.tech/">Good for People of Color</a></li>
-            <li><a href="https://techinclusion.co/#">Tech Inclusion</a></li>
-            <li><a href="http://www.code2040.org/">Code2040</a></li>
-            <li><a href="http://www.blackgirlscode.com/">Black Girls Code</a></li>
-            <li><a href="http://dev.ladieslearningcode.com/">Ladies Learning Code</a></li>
-            </ul>
+                <h3 className="card-title">Check Them Out</h3>
+                <ul>
+                    <li><a href="http://goodforpocin.tech/">Good for People of Color</a></li>
+                    <li><a href="https://techinclusion.co/#">Tech Inclusion</a></li>
+                    <li><a href="http://www.code2040.org/">Code2040</a></li>
+                    <li><a href="http://www.blackgirlscode.com/">Black Girls Code</a></li>
+                    <li><a href="http://dev.ladieslearningcode.com/">Ladies Learning Code</a></li>
+                </ul>
 
             </div>
         );
