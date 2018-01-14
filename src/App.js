f3c3279939c97581d72af971ef923c3fcdc5ecb0
flytewizard <domo.charlebois@gmail.com>
2018-01-14 01:11:12 -0800

    About Github Button

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         
         <header className="App-header">
           <Link to='/'><img src={p} className="App-logo" alt="logo" /></Link>
-          <p className="App-nav">About</p>
+          <a href="https://github.com/FlyteWizard/whatthetech" target="_blank" className="App-nav Link">Github</a>
         </header>
       
         <Switch>
