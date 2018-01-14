208cfb4df81bf7ecb3985d1be0aed1b6d3cdce05
sophia <ceasings@gmail.com>
2018-01-14 10:56:32 -0800

    updated landing page text (again)

--- src/Landing.js
+++ src/Landing.js
@@ -20,7 +20,8 @@ class Landing extends Component {
             <h1 className="App-title">Privilege Walk</h1>
             <p className="App-intro">
               This activity forces participants to confront the ways in which society privileges some individuals over others.
-              Privilege is often invisible, yet it can have a significant influence on our everyday lives.
+              Privilege is often invisible, yet it has the potential to shape every aspect our everyday lives.
+              As our lives become increasingly intertwined with technology, there is a growing need to raise awareness about these issues in the tech community.
             </p>
 
 
