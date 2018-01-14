6de3d9f6916d033de8abf49ca030f7400735711e
Macguire Rintoul <mrintoul@users.noreply.github.com>
2018-01-14 02:35:45 -0800

    3 hours of work

--- src/Start.js
+++ src/Start.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import fire from './fire';
 import { Link, Redirect } from 'react-router-dom';
 
+var usersinroom;
 
 class Start extends Component {
    constructor(props) {
@@ -16,7 +17,7 @@ class Start extends Component {
         /* Create reference to users in Firebase Database */
         var usersRef = fire.database().ref("users");
         var username = "";
-
+        
     }
       
     submitForm = (e) => {
@@ -51,16 +52,41 @@ class Start extends Component {
 
   render() {
       
+      // Add ourselves to presence list when online.
+      var listRef = fire.database().ref("usersinroom");
+      var presenceRef = fire.database().ref("usersinroom/" + localStorage.getItem("username"));
+      var currentUser = presenceRef.push(localStorage.getItem("username"));
+      presenceRef.on("value", function(snap) {
+          console.log(snap.val());
+          presenceRef.onDisconnect().remove();
+      });
+      listRef.on("value", function(snap) {
+          console.log("Online:" + snap.numChildren());
+          usersinroom = snap.numChildren();
+      });
+      
+      /*
+      fire.database().ref("usersinroom").on('value', function(snapshot) {
+            usersinroom = snapshot.val();
+          console.log(usersinroom);
+        });
+        console.log(usersinroom);
+        if (usersinroom == 10) {
+            console.log("ten users reached, ready to start");
+        }
+      */
+      
       const { from } = this.props.location.state || '/'
       const { fireRedirect } = this.state
 
     return (
+        
         <div className="Start">
             <div className="start-card">
                 <h3 className="card-title">Waiting for others</h3>
                 <p className="card-intro">Need 10 participants</p>
 
-                <h1 className="num-participants"> X </h1>
+                <h1 className="num-participants"> X {usersinroom} </h1>
 
                 <form className="start-form" onSubmit={this.submitForm.bind(this)}>
                     <input className="start-name" type="text" placeholder="Your display name" ref={ el => this.inputEl = el }/>
