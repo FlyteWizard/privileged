59977dd7bc14bfaf69be303bc1e4dbbce16eb2c8
flytewizard <domo.charlebois@gmail.com>
2018-01-14 10:00:09 -0800

    Made View Data a string.

--- src/End.js
+++ src/End.js
@@ -11,7 +11,7 @@ const data = {
   labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
   datasets: [
     {
-      label: View Data,
+      label: "View Data",
       backgroundColor: [
         "#82A19A",
         "#B5CDBF ",
